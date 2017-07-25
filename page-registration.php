ea0b9a6f3398fc6b371b66f86885cb0efd7eefc7
carlyn <carlyns@gmail.com>
2017-07-25 12:11:12 -0500

    pricing change
    batizado 2017

--- page-registration.php
+++ page-registration.php
@@ -44,25 +44,25 @@
 
 			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="VF6QP6WFNY5NE">
+				<input type="hidden" name="hosted_button_id" value="HLGEMHLP5JYWN">
 				<table>
 				<tr><td><input type="hidden" name="on0" value="Pricing">Pricing</td></tr><tr><td><select name="os0">
-					<option value="Full Event - Adult">Full Event - Adult $110.00 USD</option>
+					<option value="Full Event - Adult">Full Event - Adult $130.00 USD</option>
 					<option value="Full Event - Kids">Full Event - Kids $65.00 USD</option>
 					<option value="Instructor Rate">Instructor Rate $110.00 USD</option>
 					<option value="Professor Rate">Professor Rate $75.00 USD</option>
 				</select> </td></tr>
-				<tr><td><input type="hidden" name="on1" value="Shirt Cut">Shirt Cut</td></tr><tr><td><select name="os1">
-					<option value="Women's">Women's </option>
-					<option value="Men's">Men's </option>
-					<option value="Kids">Kids </option>
-				</select> </td></tr>
-				<tr><td><input type="hidden" name="on2" value="Enter: XS, S, M, L, XL, XXL">Enter: XS, S, M, L, XL, XXL</td></tr><tr><td><input type="text" name="os2" maxlength="200"></td></tr>
 				</table>
 				<input type="hidden" name="currency_code" value="USD">
 				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 			</form>
+
+
+
+
+
+
 
 
 
